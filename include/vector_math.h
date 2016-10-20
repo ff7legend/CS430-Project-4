b861b3132f12c9b319bd1db595c25bd2682e9045
Cameron Wesley Gaskin <cg695@tatooine.(none)>
2016-10-19 23:29:20 -0700

    updated vector_math.h

--- include/vector_math.h
+++ include/vector_math.h
@@ -9,11 +9,23 @@
 #include <math.h>
 
 
-typedef double V3[3];  
+typedef double V3[3];   // represents a 3d vector
 
 
 static inline double sqr(double v) {
     return v*v;
+}
+
+static inline void v3_zero(V3 vector) {
+    vector[0] = 0;
+    vector[1] = 0;
+    vector[2] = 0;
+}
+
+static inline void v3_copy(V3 from, V3 to) {
+    to[0] = from[0];
+    to[1] = from[1];
+    to[2] = from[2];
 }
 
 static inline void normalize(double *v) {
@@ -41,9 +53,9 @@ static inline void v3_sub(V3 a, V3 b, V3 c) {
 }
 
 static inline void v3_scale(V3 a, double s, V3 b) {
-    b[0] = s * a[0]; 
-    b[1] = s * a[1]; 
-    b[2] = s * a[2]; 
+    b[0] = s * a[0];
+    b[1] = s * a[1];
+    b[2] = s * a[2];
 }
 
 static inline double v3_dot(V3 a, V3 b) {
@@ -54,6 +66,14 @@ static inline void v3_cross(V3 a, V3 b, V3 c) {
     c[0] = a[1]*b[2] - a[2]*b[1];
     c[1] = a[2]*b[0] - a[0]*b[2];
     c[2] = a[0]*b[1] - a[1]*b[0];
+}
+
+static inline void v3_reflect(V3 v, V3 n, V3 v_r) {
+    normalize(n);
+    double scalar = 2.0 * v3_dot(n, v);
+    V3 tmp_vector;
+    v3_scale(n, scalar, tmp_vector);
+    v3_sub(v, tmp_vector, v_r);
 }
 
 
